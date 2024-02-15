9d652b1fa07663231023246f72b947598cf2586b
SUBASH-RANABHAT <subashranabhat146@gmail.com>
2024-02-15 18:10:12 +0545

    first push

---
+++ app/cart/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { remove } from "@/redux/Cartslice";
+import Image from "next/image";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const page = () => {
+  const dispatch = useDispatch();
+  const cart_item = useSelector((state) => state.cart);
+
+  const handleRemove = (id) => {
+    dispatch(remove(id));
+  };
+
+  return (
+    <div>
+      <h3>Cart Page</h3>
+      <div>
+        {cart_item.map((item) => (
+          <div className="flex gap-5 justify-between items-center bg-white m-2">
+            <Image src={item.image} alt="image" height={100} width={100} />
+            <p>{item.title} </p>
+            <p>{item.price} </p>
+            <button
+              className="rounded-md bg-red-500 px-4 py-3"
+              onClick={() => handleRemove(item.id)}
+            >
+              Remove{" "}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default page;
